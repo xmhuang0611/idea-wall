c5ab559972b7d45130aa1a9d43112b42208a9eb2
huang minghui <1920990553@qq.com>
2025-05-23 20:20:48 +0800

    idea actions

--- components/idea-wall-ui/src/app/components/idea-wall/idea-wall.component.ts
+++ components/idea-wall-ui/src/app/components/idea-wall/idea-wall.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router, ActivatedRoute } from '@angular/router';
@@ -31,6 +31,9 @@ import { FeelingUtilService } from '../../shared/services/feeling-util.service';
 import { ApiResponse } from '../../shared/models/api-response.model';
 import { SessionReviewFormComponent } from '../session-review-form/session-review-form.component';
 import { IdeaStatus } from '../../models/idea.model';
+import { MenuModule } from 'primeng/menu';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { OverlayPanel } from 'primeng/overlaypanel';
 
 interface Topic {
   name: string;
@@ -60,13 +63,17 @@ interface Topic {
     IdeaDetailsComponent,
     ConfirmDialogModule,
     IdeaHistoryComponent,
-    SessionReviewFormComponent
+    SessionReviewFormComponent,
+    MenuModule,
+    OverlayPanelModule
   ],
   providers: [ConfirmationService],
   templateUrl: './idea-wall.component.html',
   styleUrls: ['./idea-wall.component.scss']
 })
 export class IdeaWallComponent implements OnInit {
+  @ViewChild('actionsMenu') actionsMenuRef!: OverlayPanel;
+  
   ideas: Idea[] = [];
   currentUserId: string = '';
   isAdmin: boolean = false;
@@ -109,6 +116,8 @@ export class IdeaWallComponent implements OnInit {
   // Session Review
   sessionReviewDialogVisible = false;
   selectedSessionIdeaId: string | null = null;
+
+  selectedIdeaForActions: Idea | null = null;
 
   constructor(
     private ideaService: IdeaService,
@@ -591,5 +600,23 @@ export class IdeaWallComponent implements OnInit {
    */
   navigateToSessionReview(ideaId: string): void {
     this.router.navigate(['/session-review', ideaId]);
+  }
+
+  onIdeaActions(event: any, idea: Idea) {
+    this.selectedIdeaForActions = idea;
+    event.stopPropagation();
+  }
+
+  onIdeaActionsClose() {
+    this.selectedIdeaForActions = null;
+  }
+
+  hasActions(idea: Idea): boolean {
+    return this.canSubmitToSession(idea) || this.canEditIdea(idea);
+  }
+
+  showActionsMenu(event: any, idea: Idea) {
+    this.selectedIdeaForActions = idea;
+    this.actionsMenuRef.show(event);
   }
 } 
